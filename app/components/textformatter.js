b7ab770e6c50d14f8d7ef620692773fa3fa98440
Chris Han <xiphux@gmail.com>
2015-07-16 22:45:27 -0500

    Refactor text formatter service a bit

--- app/components/textformatter.js
+++ app/components/textformatter.js
@@ -5,17 +5,16 @@ angular.module('codex.textFormatter', [])
 .factory('textFormatterService', function() {
 	var service = {
 		padDenseText: function(content) {
+			// pad out dense text with blank lines
 			return content.replace(/([^\w\s,]) *\n([A-Z\t\"]| {3,})/g, "$1\n\n$2")
 		},
 		
 		compactBlankLines: function(content) {
+			// trim out extraneous blank lines
 			return content.replace(/([^\n])(\n\s*){2,}\n(\s*[^\n])/g, "$1\n\n$3");
 		},
 		
-		unwrap: function(content) {
-			
-			var unwrapped = content;
-			
+		getUnwrapMetrics: function(content) {
 			var lines = this.compactBlankLines(content).split("\n");
 			
 			var lineCount = lines.length;
@@ -39,26 +38,58 @@ angular.module('codex.textFormatter', [])
 				}
 			}
 			
-			var averageLength = (characterCount/contentLineCount) | 0;
-			var spaceToLineRatio = spaceEndedLineCount * 1.0 / contentLineCount;
+			var averageLineLength = (characterCount/contentLineCount) | 0;
+			var trailingSpaceRatio = spaceEndedLineCount * 1.0 / contentLineCount;
+			var indentedLineRatio = indentedLineCount * 1.0 / contentLineCount;
+			var blankLineRatio = (lineCount - contentLineCount) * 1.0 / lineCount;
 			
-			if ((spaceToLineRatio > 0.6) && (spaceToLineRatio < 0.95)) {
+			return {
+				averageLineLength: averageLineLength,
+				trailingSpaceRatio: trailingSpaceRatio,
+				indentedLineRatio: indentedLineRatio,
+				blankLineRatio: blankLineRatio
+			};
+		},
+		
+		unwrapTrailingSpaces: function(content) {
+			// unwrap by using the ending space as an indication of a continuing line
+			return content.replace(/ \n/g, ' ');
+		},
+		
+		unwrapIndentedLines: function(content) {
+			// unwrap by using indents to find paragraphs
+			return content.replace(/ *\n(\S)/g, " $1");
+		},
+		
+		unwrapBlankLines: function(content) {
+			// unwrap by using blank lines to find paragraphs
+			return content.replace(/([^\n]) *\n([^\r\s])/g, "$1 $2");
+		},
+		
+		unwrapLineWidths: function(content, averageLength) {
+			// unwrap by average line width
+			var rgx = new RegExp("([^\n]{" + averageLength.toString() + ",})\n", "g");
+			return content.replace(rgx, "$1 ").replace(/([\w,]) {2,}([\w,])/, "$1 $2");
+		},
+		
+		unwrap: function(content) {
+			
+			var unwrapped = content;
+			
+			var metrics = this.getUnwrapMetrics(content);
+			
+			if ((metrics.trailingSpaceRatio > 0.6) && (metrics.trailingSpaceRatio < 0.95)) {
 				// text has lots of lines that end in spaces
-				// might be unwrappable by using the ending space as an indication of a continuing line
-				unwrapped = unwrapped.replace(/ \n/g, ' ');
-			} else if ((indentedLineCount * 1.0 / contentLineCount) > 0.2) {
+				unwrapped = this.unwrapTrailingSpaces(unwrapped);
+			} else if (metrics.indentedLineRatio > 0.2) {
 				// text appears to have a number of indented lines
-				// try to use indents to find paragraphs
-				unwrapped = unwrapped.replace(/ *\n(\S)/g, " $1");
-			} else if (((lineCount - contentLineCount) * 1.0 / lineCount) > 0.1) {
+				unwrapped = this.unwrapIndentedLines(unwrapped);
+			} else if (metrics.blankLineRatio > 0.1) {
 				// text appears to have a number of blank lines
-				// try to use blank lines to find paragraphs
-				unwrapped = unwrapped.replace(/([^\n]) *\n([^\r\s])/g, "$1 $2");
-			} else if (averageLength > 1) {
+				unwrapped = this.unwrapBlankLines(unwrapped);
+			} else if (metrics.averageLineLength > 1) {
 				// try to unwrap by guessing the width of lines
-				var rgx = new RegExp("([^\n]{" + averageLength.toString() + ",})\n", "g");
-				unwrapped = unwrapped.replace(rgx, "$1 ");
-				unwrapped = unwrapped.replace(/([\w,]) {2,}([\w,])/, "$1 $2");
+				unwrapped = this.unwrapLineWidths(unwrapped, metrics.averageLineLength);
 			}
 			
 			// preserve breaklines that may have been accidentally wrapped up
@@ -82,7 +113,6 @@ angular.module('codex.textFormatter', [])
 			if (!content) {
 				return '';
 			}
-			
 			
 			content = content
 				.replace(/\r\n/g, "\n")   	// use unix line breaks
