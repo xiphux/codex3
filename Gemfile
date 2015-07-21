c6c98d4c284f55cbc08a57635ac3e7b752b99f85
Chris Han <xiphux@gmail.com>
2015-07-21 18:31:59 -0500

    Move sql server config to separate config file

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem "sinatra-contrib"
 gem "oj"
 gem "data_mapper"
 gem "dm-mysql-adapter"
--- main.rb
+++ main.rb
@@ -1,12 +1,18 @@
 require 'oj'
 require 'sinatra'
+require 'sinatra/config_file'
 require 'data_mapper'
 
+config_file 'config/config.yml'
+
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(
-	:default,
-	'mysql://codex:codex@localhost/codex'
-)
+DataMapper.setup(:default, {
+	:adapter => settings.adapter,
+	:database => settings.database,
+	:username => settings.username,
+	:password => settings.password,
+	:host => settings.host
+})
 
 require './models'
 require './routes'
